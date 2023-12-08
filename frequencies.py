86162fb965990664025338dfb96a7dda84e748e1
Deyu Li <k21081622@kcl.ac.uk>
2023-12-08 12:41:35 +0000

    complete frequencies

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,16 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
+from collections import Counter
+
 
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    positive_list = [int(abs(number)) if isinstance(number, (int, float)) else number for number in items]
+
+    string_list = [str(item) for item in positive_list]
+
+    frequencies = dict(Counter(string_list))
+
     return frequencies
